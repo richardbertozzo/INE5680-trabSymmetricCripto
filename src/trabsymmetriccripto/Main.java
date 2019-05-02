39a507b8c6a1fc7d3e3650429b335469a0c66496
Richard Bertozzo <richardjrbertozzo@gmail.com>
2019-05-02 19:34:57 -0300

    create main of project

---
+++ src/trabsymmetriccripto/Main.java
@@ -0,0 +1,24 @@
+package trabsymmetriccripto;
+
+public class Main {
+    
+    private static void generateKey() {
+        
+    }
+    
+    private static void saveKey() {
+        
+    }
+    
+    private static void cifrarMsg() {
+        
+    }
+    
+    private static String decifrarMsg() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.err.println("oi");
+    }
+}
